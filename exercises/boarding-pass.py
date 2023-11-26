4ce9da0779e194ca8559ede7d3c0605da33d84df
josfam <josephamukun@tutanota.com>
2023-11-26 14:03:20 +0300

    Add exercises

---
+++ exercises/boarding-pass.py
@@ -0,0 +1,4 @@
+# one passenger on a certain flight
+passenger_info = ["KL 0862", "Jordan Li", "tokyo/nrt", "amsterdam/ams", "26NOV23", "a1", "a", "C", "11:30AM"]
+
+# > Only add code below this line
---
+++ exercises/marie.py
@@ -0,0 +1,20 @@
+import pprint
+
+# String representation of a student
+student = "00678, Marie Moreau, Lamplighter School of Crime-fighting, Godolkin"
+
+# > Only add code below this line
+
+# > Only add code above this line
+
+student = {
+	"00678": {
+		"first_name": first_name,
+		"last_name": last_name,
+		"university": uni,
+		"college": college,
+	}
+}
+
+pprint.pprint(student)
+
